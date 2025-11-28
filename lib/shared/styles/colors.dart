51b77364a9ff7de49f30360c44a8f6e9eae8aa00
Mijo-sh <majedaalshalabi207@gmail.com>
2025-11-28 20:06:58 +0200

    ✨feat: set colors for app

---
+++ lib/shared/styles/colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  // for light mode
+  static const Color oxfordBlueLight = Color(0xff0f1e3f);
+  static const Color jordyBlueLight = Color(0xff213a56);
+  static const Color tanLight = Color(0xff997953);
+  static const Color burlywoodLight = Color(0xffcdaa80);
+
+  // for dark mode
+  static const Color oxfordBlueDark = Color(0xff102134);
+  static const Color jordyBlueDark = Color(0xff1a293f);
+  static const Color grayDark = Color(0xff404751);
+  static const Color tanDark = Color(0xff896f3d);
+
+  // basic colors
+  static const Color white = Color(0xffffffff);
+  static const Color black = Color(0xff000000);
+}
